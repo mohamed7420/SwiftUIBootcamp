dbab3e8d25bbb5f6ea492c330647f6c2d7c12212
mohamed osama <mohamed.osama9812@gmail.com>
2023-12-12 22:51:27 +0200

    Initial Commit

---
+++ SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Mohamed Osama on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
