9662663a95bc189166924e1e62eae370b509274f
mohamed osama <mohamed.osama9812@gmail.com>
2024-01-04 01:13:57 +0200

    observed object and state object in swiftui

---
+++ SwiftUIExamples/ObservedStateObjectBootcamp.swift
@@ -0,0 +1,96 @@
+//
+//  ObservedStateObjectBootcamp.swift
+//  SwiftUIExamples
+//
+//  Created by Mohamed Osama on 04/01/2024.
+//
+
+import SwiftUI
+
+struct Fruit: Identifiable {
+    let id: String = UUID().uuidString
+    let name: String
+    let count: Int
+}
+
+class FruitViewModel: ObservableObject {
+    @Published var fruits: [Fruit] = []
+    @Published var isLoading: Bool = false
+
+    init() {
+        Task { await loadAllFruits() }
+    }
+
+    public func loadAllFruits() async {
+        isLoading = true
+        try? await Task.sleep(nanoseconds: 2 * 1_000_000_000)
+
+        let apples = Fruit(name: "Apple", count: 5)
+        let oranges = Fruit(name: "Oranges", count: 10)
+        let bananas = Fruit(name: "Bananas", count: 25)
+
+        let newFruits = [apples, oranges, bananas]
+        fruits.append(contentsOf: newFruits)
+
+        isLoading = false
+    }
+
+}
+
+struct ObservedStateObjectBootcamp: View {
+    @StateObject var viewModel = FruitViewModel()
+
+    var body: some View {
+        NavigationView {
+            NavigationStack {
+                if viewModel.isLoading {
+                    ProgressView()
+                        .tint(.pink)
+                } else {
+                    List(viewModel.fruits) { fruit in
+                        HStack {
+                            Text("\(fruit.count)")
+                                .foregroundStyle(.pink)
+                                .bold()
+                                .underline()
+
+                            Text(fruit.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("State Objects")
+            .toolbar {
+                NavigationLink {
+                    AnotherView()
+                } label: {
+                    Image(systemName: "arrow.right")
+                        .foregroundStyle(.pink)
+                        .bold()
+                }
+
+            }
+//            .onAppear {
+//                Task {
+//                    await viewModel.loadAllFruits()
+//                }
+//            }
+        }
+    }
+}
+
+struct AnotherView: View {
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea()
+            Text("Hello, iOS Developers")
+                .foregroundStyle(.black)
+                .font(.title2)
+                .bold()
+        }
+    }
+}
+
+#Preview {
+    ObservedStateObjectBootcamp()
+}
