5266c432c0966759a6e1fc53bf02c7789e58dbd4
mohamed osama <mohamed.osama9812@gmail.com>
2024-01-17 18:10:05 +0200

    swipe actions in swiftui

---
+++ SwiftUIExamples/SwipeActionBootcamp.swift
@@ -0,0 +1,34 @@
+//
+//  SwipeActionBootcamp.swift
+//  SwiftUIExamples
+//
+//  Created by Mohamed Osama on 17/01/2024.
+//
+
+import SwiftUI
+
+struct SWipeActionBootcamp: View {
+    
+    @State var fruites: [String] = [
+        "Apple",
+        "Banana",
+        "Orange"
+    ]
+
+    var body: some View {
+        List {
+            ForEach(fruites, id: \.self) { fruit in
+                Text(fruit)
+                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                        Button("POST") {
+                            
+                        }
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    SWipeActionBootcamp()
+}
